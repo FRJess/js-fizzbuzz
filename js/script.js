0b25048049a1f443ac35940afa410d52463481b7
FR_Jess <fr_jess@icloud.com>
2022-10-13 16:28:10 +0200

    [add] replace try

--- js/script.js
+++ js/script.js
@@ -2,15 +2,26 @@
 // variabile container e limite
 const container = document.querySelector(".container");
 const limit = 105;
+// let i;
+let fizz = "fizz"
 
 // ciclo di ripetizione dei box
 for(let i = 1; i <=limit; i++){
   const box = document.createElement ("div");
   box.className = "box";
 
+  // stampo in html
+  box.innerHTML = i;
+  container.append(box);
+
   //condizione se multiplo di 3
   if(! (i % 3)){
     box.classList.add("fizz");
+    box.replace(i,fizz);
+    // console.log(i.replace:([0-9],"fizz"));
+    // let box.innerHTML = box.replace(i, "fizz");
+    // let text = document.getElementById("box").innerHTML; 
+    // document.getElementById("box").innerHTML = text.replace(i, "W3Schools");
   }
 
   //condizione se multiplo di 5
@@ -18,8 +29,6 @@ for(let i = 1; i <=limit; i++){
     box.classList.add("buzz");
   }
 
-  // stampo in html
-  box.innerHTML = i;
-  container.append(box);
+
 }
 

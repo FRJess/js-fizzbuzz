04aed8f65fd66079694b0776bbe76e8d2082e884
FR_Jess <fr_jess@icloud.com>
2022-10-13 17:14:19 +0200

    [add] comments

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
 
-// variabile container e limite
+// VARIABLES DECLARATION AND INIZIALIZATION
 const container = document.querySelector(".container");
 const limit = 105;
 
-// ciclo di ripetizione dei box
+// CYCLE
 for(let i = 1; i <=limit; i++){
   const box = document.createElement ("div");
   box.className = "box";
 
-  // aggiungo al container  
+  // add it to container  
   container.append(box);
 
-  //condizione se multiplo di 3 e di 5
+  //condition if multiple of 3 and 5
   if(i % 3 === 0 && i % 5 === 0){
     box.classList.add("fizzbuzz");
     box.innerHTML = "FizzBuzz";
   }
 
-  //condizione se multiplo di 5
+  //condition if multiple of 5
   else if(!(i % 5)){
     box.classList.add("buzz");
     box.innerHTML = "Buzz";
   }
   
-  //condizione se multiplo di 3
+  //condition if multiple of 3
   else if(!(i % 3)){
     box.classList.add("fizz");
     box.innerHTML = "Fizz";
   }
 
-
+  // condition if none of the above
   else{
     box.innerHTML = i;
   }

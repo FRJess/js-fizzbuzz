162313ea96adafe93217e191168488e1fef90c73
FR_Jess <fr_jess@icloud.com>
2022-10-13 16:58:42 +0200

    [add] conditions

--- js/script.js
+++ js/script.js
@@ -2,33 +2,37 @@
 // variabile container e limite
 const container = document.querySelector(".container");
 const limit = 105;
-// let i;
-let fizz = "fizz"
 
 // ciclo di ripetizione dei box
 for(let i = 1; i <=limit; i++){
   const box = document.createElement ("div");
   box.className = "box";
 
-  // stampo in html
-  box.innerHTML = i;
+  // aggiungo al container  
   container.append(box);
 
-  //condizione se multiplo di 3
-  if(! (i % 3)){
-    box.classList.add("fizz");
-    box.replace(i,fizz);
-    // console.log(i.replace:([0-9],"fizz"));
-    // let box.innerHTML = box.replace(i, "fizz");
-    // let text = document.getElementById("box").innerHTML; 
-    // document.getElementById("box").innerHTML = text.replace(i, "W3Schools");
+  //condizione se multiplo di 3 e di 5
+  if(i % 3 === 0 && i % 5 === 0){
+    box.classList.add("fizzbuzz");
+    box.innerHTML = "FizzBuzz";
   }
 
   //condizione se multiplo di 5
-  if(! (i % 5)){
+  else if(!(i % 5)){
     box.classList.add("buzz");
+    box.innerHTML = "Buzz";
+  }
+  
+  //condizione se multiplo di 3
+  else if(!(i % 3)){
+    box.classList.add("fizz");
+    box.innerHTML = "Fizz";
   }
 
+
+  else{
+    box.innerHTML = i;
+  }
 
 }
 
